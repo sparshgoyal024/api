0f49b49fc2570f80ccf10e93ec81abf7fdff0282
sparshgoyal024 <58138831+sparshgoyal024@users.noreply.github.com>
2021-11-01 19:42:20 +0530

    Update main.js

--- web/main.js
+++ web/main.js
@@ -9,7 +9,7 @@ function loadAuthClient () {
 // [END load_auth2_library]
 
 // [START init_google_auth]
-function initGoogleAuth (clientId = '500140982586-cvlvqe74fb5lg04peq2de65a16mkcp3s.apps.googleusercontent.com') {
+function initGoogleAuth (clientId = 'Replace Client ID') {
   gapi.auth2.init({
     client_id: clientId,
     scope: 'https://www.googleapis.com/auth/userinfo.email'
@@ -35,13 +35,13 @@ function signIn () {
 // [END user_signin]
 
 // [START send_sample_request]
-function endpointApiRequest(projectId = 'ca-lab') {
+function endpointApiRequest(projectId = 'replace-projectId') {
   var user = gapi.auth2.getAuthInstance().currentUser.get();
 
   console.log(user);
   var idToken = user.getAuthResponse().id_token;
     console.log(idToken);
-  var endpoint = `https://ca-lab.appspot.com/_ah/api/email/v1/get/email`;
+  var endpoint = `Add Cloud Endpoint URL with Path`;
 
   var xhr = new XMLHttpRequest();
     console.log(xhr);
@@ -56,23 +56,19 @@ function endpointApiRequest(projectId = 'ca-lab') {
   xhr.send();
 }
 
-function apiGatewayRequest(projectId = 'ca-lab') {
+function apiGatewayRequest(projectId = 'replace-projectId') {
   var user = gapi.auth2.getAuthInstance().currentUser.get();
 
   //console.log(user);
   var idToken = user.getAuthResponse().id_token;
   //  console.log(idToken);
-  var endpoint = `https://ca-gateway-6drf0zzu.uc.gateway.dev/getUser`;
+  var endpoint = `https://ca-gateway-6drf0zzu.uc.gateway.dev/getUser`;  //Replace API Gateway host url here, keep https:// and getUser as is.
 
   var xhr = new XMLHttpRequest();
   //  console.log(xhr);
   xhr.open('GET', endpoint + '?access_token=' + encodeURIComponent(idToken));
-
-  console.log("xhr.responseText");
-
-  console.log(xhr.responseText);
-
-  console.log("xhr.responseText");
+  xhr.withCredentials = true;
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencode');
 
   xhr.onreadystatechange = function () {
     if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
