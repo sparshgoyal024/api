0bad7296e754408391529f0e89f230fd5a518f61
sparshgoyal024 <58138831+sparshgoyal024@users.noreply.github.com>
2021-11-27 09:51:03 +0530

    Headers Added

--- endpoints/main.py
+++ endpoints/main.py
@@ -7,6 +7,7 @@ from endpoints import remote
 # [END endpoints_email_api_imports]
 class EmailRequest(messages.Message):
     message = messages.StringField(1)
+    
 class EmailResponse(messages.Message):
     message = messages.StringField(1)
     
@@ -19,12 +20,20 @@ class EmailApi(remote.Service):
     
     def get_user_email(self, request):
         user = endpoints.get_current_user()
+        headers = {
+            'Access-Control-Allow-Origin':'*',
+            'Access-Control-Allow-Methods': 'GET',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': '3600'
+        }
+         
+        print(headers)
+        
         # If there's no user defined, the request was unauthenticated, so we
         # raise 401 Unauthorized.
         if not user:
             raise endpoints.UnauthorizedException
         return EmailResponse(message=user.email())
-# [END endpoints_email_api_class]
-# [START endpoints_api_server]
+
+    
 api = endpoints.api_server([EmailApi])
-# [END endpoints_api_server]
