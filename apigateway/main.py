0dc39d9a0970b60176135a3e16b4ac9d94c71c41
sparshgoyal024 <58138831+sparshgoyal024@users.noreply.github.com>
2021-11-27 09:57:45 +0530

    Update main.py

--- apigateway/main.py
+++ apigateway/main.py
@@ -1,9 +1,19 @@
-from flask import Flask,request
+from flask import Flask, request
 from flask_cors import CORS
+
 app = Flask(__name__)
 CORS(app)
-@app.route('/', methods=['GET'])
+
+
+@app.route("/", methods=["GET"])
 def hello():
-    """Passing parameters below."""
-    who=request.args.get('who','CA')
-    return f'Welcome {who}!\n'
+
+    headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Max-Age": "3600",
+    }
+    print(headers)
+
+    return "Welcome CA"
